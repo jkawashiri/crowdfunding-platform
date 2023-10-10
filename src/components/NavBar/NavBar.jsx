24e1c01134a372d9198679af8fbad60448d7bda4
jkawashiri <jkawashiri5@gmail.com>
2023-10-10 14:30:55 -0700

    adds dropdown functionality for search bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,29 +7,44 @@ export default function NavBar({user, setUser}) {
     const [search, setSearch] = useState('')
     const [results, setResults] = useState([])
     const [clicked, setClicked] = useState(true)
+    const [showResults, setShowResults] = useState(false)
+    const [recentSearch, setRecentSearch] = useState('')
 
     function onClick() {
         setClicked(clicked => !clicked)
-        setSearch('')
-        setResults([])
+        if (!clicked) {
+            setSearch('')
+            setResults([])
+            setShowResults(false)
+        }
     }
 
     function onChange(evt) {
-        setSearch(evt.target.value)
+        const value = evt.target.value
+        setSearch(value)
+
+        if (value.length > 2) {
+            fetchSearchResults(value)
+        } else {
+            setResults([])
+            setShowResults(false)
+        }
     }
 
-    async function onSearch() {
+    async function fetchSearchResults(query) {
+        if (!query || query.trim() === '') return
         try {
-            const response = await fetch(`http://localhost:3001/search?q=${search}`);
-            const data = await response.json();
+            const response = await fetch(`http://localhost:3001/search?q=${query}`)
+            const data = await response.json()
             if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+                throw new Error(`HTTP error! Status: ${response.status}`)
             }
-            setResults(data);
+            setResults(data)
+            setShowResults(true)
+            setRecentSearch(query)
         } catch (err) {
-            console.error("Error fetching search results:", err);
+            console.error("Error fetching search results:", err)
         }
-        setSearch('')
     }
 
     function handleLogOut() {
@@ -48,15 +63,20 @@ export default function NavBar({user, setUser}) {
             :
                 <>
                     <input type="text" placeholder="Search for Campaigns" value={search} onChange={onChange} />
-                    <button onClick={onSearch}>Search</button>
+                    <button onClick={() => fetchSearchResults(search)}>Search</button>
                     <Link onClick={onClick}><MdOutlineCancel /></Link>
+                    { showResults && ( 
+                        results.length > 0 ?
+                            <ul>
+                                {results.map((campaign, idx) => (
+                                    <Link to={`/campaigns/${campaign._id}`} key={idx}>{campaign.name}</Link>
+                                ))}
+                            </ul>
+                        : 
+                            <div>No results found for "{recentSearch}"</div>
+                    )}
                 </>
             }
-            <ul>
-                {results.map(campaign => (
-                    <li key={campaign._id}>{campaign.name}</li>
-                ))}
-            </ul>
         </nav>
     )
 }
