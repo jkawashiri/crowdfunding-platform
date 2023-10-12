63b1a37c52e06c326840257482ffbc70f160076c
jkawashiri <jkawashiri5@gmail.com>
2023-10-12 12:14:59 -0700

    adds horizontal scroll menus for each category on home page

--- src/components/Campaign/Campaign.jsx
+++ src/components/Campaign/Campaign.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom"
+import './Campaign.css'
 
 export default function Campaign({campaign}) {
     const [year, month, day] = campaign.closeDate.split('T')[0].split('-')
     const formattedDate = `${month}/${day}/${year}`
     return (
         <>
-            <li>
-                <Link to={`/campaigns/${campaign._id}`}>
-                    {campaign.name}
-                    {campaign.description}
-                    {campaign.raiseGoal}
-                    {formattedDate}
-                </Link>
-            </li>
+            <Link to={`/campaigns/${campaign._id}`} className="campaign-container">
+                <div>{campaign.name}</div>
+                <div>{campaign.description}</div>
+                <div>{campaign.raiseGoal}</div>
+                <div>{formattedDate}</div>
+            </Link>
         </>
     )
 }   
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import Campaign from "../../components/Campaign/Campaign"
 import * as campaignsAPI from '../../utilities/campaigns-api';
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import './HomePage.css'
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 export default function HomePage({campaigns, setCampaigns}) {
     useEffect(function() {
@@ -10,6 +12,26 @@ export default function HomePage({campaigns, setCampaigns}) {
         }
         getCampaigns()
     }, [])
+
+    const trendingRef = useRef(null)
+    const recentlyLaunchedRef = useRef(null)
+    const closingSoonRef = useRef(null)
+    const closedCampaignsRef = useRef(null)
+    const allCampaignRef = useRef(null)
+
+    function slideLeft(sliderRef) {
+        const slider = sliderRef.current
+        if (slider) {
+            slider.scrollLeft -= 500
+        }
+    }
+
+    function slideRight(sliderRef) {
+        const slider = sliderRef.current
+        if (slider) {
+            slider.scrollLeft += 500
+        }
+    }
 
     const recentlyLaunched = campaigns.filter((campaign) => {
         const dateLaunched = new Date(campaign.createdAt)
@@ -42,39 +64,59 @@ export default function HomePage({campaigns, setCampaigns}) {
     return (
         <>
             <h3>Trending Campaigns</h3>
-            <ul>
-                {trendingCampaigns.map((campaign, idx) => (
-                    <Campaign campaign={campaign} key={idx} />
-                ))}
-            </ul>
+            <div className="campaign-list-container">
+                <MdChevronLeft className="arrow" onClick={() => slideLeft(trendingRef)} />
+                <div ref={trendingRef} className="campaign-horizontal-scroll">
+                    {trendingCampaigns.map((campaign, idx) => (
+                        <Campaign campaign={campaign} key={idx} />
+                    ))}
+                </div>
+                <MdChevronRight className="arrow" onClick={() => slideRight(trendingRef)} />
+            </div>
 
             <h3>Recently Launched</h3>
-            <ul>
-                {recentlyLaunched.map((campaign, idx) => (
-                    <Campaign campaign={campaign} key={idx} />
-                ))}
-            </ul>
+            <div className="campaign-list-container">
+                <MdChevronLeft className="arrow" onClick={() => slideLeft(recentlyLaunchedRef)} />
+                <div ref={recentlyLaunchedRef} className="campaign-horizontal-scroll">
+                    {recentlyLaunched.map((campaign, idx) => (
+                        <Campaign campaign={campaign} key={idx} />
+                    ))}
+                </div>
+                <MdChevronRight className="arrow" onClick={() => slideRight(recentlyLaunchedRef)} />
+            </div>
 
             <h3>Closing Soon</h3>
-            <ul>
-                {closingSoon.map((campaign, idx) => (
-                    <Campaign campaign={campaign} key={idx} />
-                ))}
-            </ul>
+            <div className="campaign-list-container">
+                <MdChevronLeft className="arrow" onClick={() => slideLeft(closingSoonRef)} />
+                <div ref={closingSoonRef} className="campaign-horizontal-scroll">
+                    {closingSoon.map((campaign, idx) => (
+                        <Campaign campaign={campaign} key={idx} />
+                    ))}
+                </div>
+                <MdChevronRight className="arrow" onClick={() => slideRight(closingSoonRef)} />
+            </div>
 
             <h3>Closed Campaigns</h3>
-            <ul>
-                {closedCampaigns.map((campaign, idx) => (
-                    <Campaign campaign={campaign} key={idx} />
-                ))}
-            </ul>
+            <div className="campaign-list-container">
+                <MdChevronLeft className="arrow" onClick={() => slideLeft(closedCampaignsRef)} />
+                <div ref={closedCampaignsRef} className="campaign-horizontal-scroll">
+                    {closedCampaigns.map((campaign, idx) => (
+                        <Campaign campaign={campaign} key={idx} />
+                    ))}
+                </div>
+                <MdChevronRight className="arrow" onClick={() => slideRight(closedCampaignsRef)} />
+            </div>
 
             <h3>All Campaigns</h3>
-            <ul>
-                {campaigns.map((campaign, idx) => (
-                    <Campaign campaign={campaign} key={idx} />
-                ))}
-            </ul>
+            <div className="campaign-list-container">
+                <MdChevronLeft className="arrow" onClick={() => slideLeft(allCampaignRef)} />
+                <div ref={allCampaignRef} className="campaign-horizontal-scroll">
+                    {campaigns.map((campaign, idx) => (
+                        <Campaign campaign={campaign} key={idx} />
+                    ))}
+                </div>
+                <MdChevronRight className="arrow" onClick={() => slideRight(allCampaignRef)} />
+            </div>
         </>
     )
 }
