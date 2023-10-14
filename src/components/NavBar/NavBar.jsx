58f2519bae39a9e0e54f04f046e002e9897e9088
jkawashiri <jkawashiri5@gmail.com>
2023-10-13 18:03:32 -0700

    adds animations for search bar and formats results

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,53 +1,9 @@
 import { Link } from "react-router-dom"
 import * as userService from '../../utilities/users-service'
-import { useState } from "react"
-import { MdSearch, MdOutlineClose } from "react-icons/md"
 import './NavBar.css'
+import SearchBar from "../SearchBar/SearchBar"
 
 export default function NavBar({user, setUser}) {
-    const [search, setSearch] = useState('')
-    const [results, setResults] = useState([])
-    const [clicked, setClicked] = useState(true)
-    const [showResults, setShowResults] = useState(false)
-    const [recentSearch, setRecentSearch] = useState('')
-
-    function onClick() {
-        setClicked(clicked => !clicked)
-        if (!clicked) {
-            setSearch('')
-            setResults([])
-            setShowResults(false)
-        }
-    }
-
-    function onChange(evt) {
-        const value = evt.target.value
-        setSearch(value)
-
-        if (value.length > 2) {
-            fetchSearchResults(value)
-        } else {
-            setResults([])
-            setShowResults(false)
-        }
-    }
-
-    async function fetchSearchResults(query) {
-        if (!query || query.trim() === '') return
-        try {
-            const response = await fetch(`http://localhost:3001/search?q=${query}`)
-            const data = await response.json()
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`)
-            }
-            setResults(data)
-            setShowResults(true)
-            setRecentSearch(query)
-        } catch (err) {
-            console.error("Error fetching search results:", err)
-        }
-    }
-
     function handleLogOut() {
         userService.logOut()
         setUser(null)
@@ -60,25 +16,7 @@ export default function NavBar({user, setUser}) {
                 </div>
             </Link>
             <Link to={user ? "/campaigns/new" : "/auth"}>Create a Campaign</Link>
-            { clicked ?
-                <Link onClick={onClick}>Search <MdSearch className="search-icon" /></Link>
-            :
-                <>
-                    <input type="text" placeholder="Search for Campaigns" value={search} onChange={onChange} />
-                    <button onClick={() => fetchSearchResults(search)}>Search</button>
-                    <Link onClick={onClick}><MdOutlineClose className="close-icon" /></Link>
-                    { showResults && ( 
-                        results.length > 0 ?
-                            <ul>
-                                {results.map((campaign, idx) => (
-                                    <Link to={`/campaigns/${campaign._id}`} key={idx}>{campaign.name}</Link>
-                                ))}
-                            </ul>
-                        : 
-                            <div>No results found for "{recentSearch}"</div>
-                    )}
-                </>
-            }
+            <SearchBar />
             { user ?
                 <>
                     <span>Welcome, {user.name}</span>
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react"
+import { MdSearch, MdOutlineClose } from "react-icons/md"
+import { Link } from "react-router-dom"
+import './SearchBar.css'
+import { motion } from "framer-motion"
+import { useClickOutside } from "react-click-outside-hook"
+import MoonLoader from "react-spinners/MoonLoader"
+import SearchResult from "../SearchResult/SearchResult"
+
+export default function SearchBar() {
+    const [search, setSearch] = useState('')
+    const [results, setResults] = useState([])
+    const [clicked, setClicked] = useState(true)
+    const [showResults, setShowResults] = useState(false)
+    const [recentSearch, setRecentSearch] = useState('')
+    const [expanded, setExpanded] = useState(false)
+    const [ref, isClickedOutside] = useClickOutside()
+
+    useEffect(function() {
+        if(isClickedOutside) {
+            collapseContainer()
+            setShowResults(false)
+        }
+    }, [isClickedOutside])
+
+    function expandContainer() {
+        setExpanded(true)
+        fetchSearchResults(search)
+    }
+
+    function collapseContainer() {
+        setExpanded(false)
+    }
+
+    const containerVariants = {
+        expanded: {
+            height: "20em"
+        },
+        collapsed: {
+            height: "100%"
+        }
+    }
+
+    const containerTransition = {
+        type: 'spring',
+        damping: 22,
+        stiffness: 150
+    }
+
+    function onClick() {
+        setClicked(clicked => !clicked)
+        if (!clicked) {
+            setSearch('')
+            setResults([])
+            setShowResults(false)
+        }
+    }
+
+    function onChange(evt) {
+        const value = evt.target.value
+        setSearch(value)
+
+        if (value.length > 2) {
+            fetchSearchResults(value)
+        } else {
+            setResults([])
+            setShowResults(false)
+        }
+    }
+
+    async function fetchSearchResults(query) {
+        if (!query || query.trim() === '') return
+        try {
+            const response = await fetch(`http://localhost:3001/search?q=${query}`)
+            const data = await response.json()
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+            setResults(data)
+            setShowResults(true)
+            setRecentSearch(query)
+        } catch (err) {
+            console.error("Error fetching search results:", err)
+        }
+    }
+    return (
+        <>
+            { clicked ?
+                <Link onClick={onClick}>Search <MdSearch className="search-icon" /></Link>
+            :
+                <motion.div 
+                    animate={expanded ? "expanded" : "collapsed"} 
+                    variants={containerVariants} 
+                    transition={containerTransition} 
+                    ref={ref}
+                    style={{transformOrigin: "top"}}
+                    className="search-container"
+                >
+                    <div className="search-bar">
+                        <input className="search-input" type="text" placeholder="Search for Campaigns" value={search} onChange={onChange} onFocus={expandContainer} />
+                        <Link onClick={() => {onClick(); collapseContainer();}}><MdOutlineClose className="close-icon" /></Link>
+                    </div>
+                    <div className="spinner-container">
+                        { expanded && showResults === false ?
+                            <MoonLoader loading color="black" size={20} />
+                        :
+                            null
+                        }
+                    </div>
+                    <div className="search-results-container">
+                        { showResults && ( 
+                            results.length > 0 ?
+                                <ul className="search-results">
+                                    {results.map((campaign, idx) => (
+                                        <SearchResult campaign={campaign} key={idx} />
+                                    ))}
+                                </ul>
+                            : 
+                                <div>No results found for "{recentSearch}"</div>
+                        )}
+                    </div>
+                </motion.div>
+            }
+        </>
+    )
+}
