cf884eebaef88834688de9189b3516c4fd769105
jkawashiri <jkawashiri5@gmail.com>
2023-10-14 15:05:30 -0700

    adds animation when hovering over campaigns on home page and search results

--- src/components/Campaign/Campaign.jsx
+++ src/components/Campaign/Campaign.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom"
 import './Campaign.css'
+import { motion } from "framer-motion"
 
 export default function Campaign({campaign}) {
     const percentageToGoal = Math.round((campaign.moneyRaised / campaign.raiseGoal) * 100)
     const formattedRaiseGoal = campaign.raiseGoal.toLocaleString()
     return (
-        <div className="campaign-container">
+        <motion.div 
+            className="campaign-container"
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        >
             <Link to={`/campaigns/${campaign._id}`} >
                 <div className="campaign-name">{campaign.name}</div>
             </Link>
             <div className="campaign-percent">{percentageToGoal}% funded</div>
             <div className="campaign-goal">${formattedRaiseGoal} raised</div>
-        </div>
+        </motion.div>
     )
 }   
--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom"
 import './SearchResult.css'
+import { motion } from "framer-motion"
 
 export default function SearchResult({campaign, onClick}) {
     const percentageToGoal = Math.round((campaign.moneyRaised / campaign.raiseGoal) * 100)
     const formattedRaiseGoal = campaign.raiseGoal.toLocaleString()
     return (
-        <div className="search-result-container">
+        <motion.div 
+            className="search-result-container"
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        >
             <Link to={`/campaigns/${campaign._id}`} className="campaign-name" onClick={onClick}>{campaign.name}</Link>
             <div className="campaign-percent">{percentageToGoal}% funded</div>
             <div className="campaign-goal">${formattedRaiseGoal} raised</div>
-        </div>
+        </motion.div>
     )
 }
