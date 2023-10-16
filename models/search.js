11775a8885d7729a48da4327acc45a73817dc07a
jkawashiri <jkawashiri5@gmail.com>
2023-10-16 16:34:00 -0700

    gets rid of unnecessary search model

--- models/search.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/crowdfunding-platform', { useNewUrlParser: true, useUnifiedTopology: true });
-// const Schema = mongoose.Schema;
-
-// const searchSchema = new Schema({
-//     item: String
-// })
-
-// module.exports = mongoose.model('Search', searchSchema)
